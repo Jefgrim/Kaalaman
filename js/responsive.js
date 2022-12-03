e4ee21d906253fe3de78a3e3a8b7a31c36fd3398
borjjay <79965207+borjjay@users.noreply.github.com>
2022-12-03 18:42:27 +0800

    responsive js comment

--- js/responsive.js
+++ js/responsive.js
@@ -1,36 +1,38 @@
   
-const  ChangeWindowSize = () =>{ 
- let hideSidebar = document.querySelector(".sidebar")
- let showSidebar = document.querySelector(".sideBar-toggle")
- let hidePostThread = document.querySelector (".rightContainer")
- let expandThread = document.querySelector (".threadContainer")
+// const  ChangeWindowSize = () =>{ 
+//  let hideSidebar = document.querySelector(".sidebar");
+//  let showSidebar = document.querySelector(".sideBar-toggle");
+//  let hidePostThread = document.querySelector (".rightContainer");
+//  let expandThread = document.querySelector (".threadContainer");
+//  let showPostThread = document.querySelector(".hidePostThreadBtn");
     
-    if (window.innerWidth < 800){
-       showSidebar.style.display ="flex";
-       hideSidebar.style.display = "none"
-       hidePostThread.style.display = "none"
-       expandThread.style.width  = "100%";
+//     if (window.innerWidth < 800){
+//        showSidebar.style.display ="flex";
+//        hideSidebar.style.display = "none";
+//        hidePostThread.style.display = "none";
+//        expandThread.style.width  = "100%";
+//        showPostThread.style.display = "flex";
 
-    }
+//     }
 
-    else {
-       showSidebar.style.display = "none";
-       hideSidebar.style.display ="flex"
-       hidePostThread.style.display = "flex"
-       expandThread.style.width = "none"
+//     else {
+//        showSidebar.style.display = "none";
+//        hideSidebar.style.display ="flex";
+//        hidePostThread.style.display = "flex";
+//        expandThread.style.width = "none";
+//        showPostThread.style.display ="none";
 
-    }
+//     }
 
-    showSidebar.addEventListener("click" ,function(){
-    hideSidebar.style.display = "flex"
-    hideSidebar.style.position = "absolute"
-    hideSidebar.style.width = "20%"
-        
-
-     })
+//     showSidebar.addEventListener("click" ,function(){
+//     hideSidebar.style.display = "flex"
+//     hideSidebar.style.position = "absolute"
+//     hideSidebar.style.width = "20%"
+    
+//      })
        
 
-   }
+//    }
 
 
 
@@ -39,4 +41,4 @@ const  ChangeWindowSize = () =>{
 
        
 
-export default ChangeWindowSize 
+// export default ChangeWindowSize 
